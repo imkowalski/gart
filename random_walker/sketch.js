81a2ae991ba65d38ff8fd1c1095bf4d7f530c4e4
Michal Kowalski <michalkowal05@gmail.com>
2022-02-10 18:59:27 +0100

    fixed wlker and added a class structure to it

--- random_walker/sketch.js
+++ random_walker/sketch.js
@@ -1,42 +1,56 @@
 const size = 400;
-let x = size / 2;
-let y = size / 2;
 const psize = 20;
-let r = 50;
-let g = 200;
-let b = 100;
-let rng = 10
+
+let a, b, c;
 
 function setup() {
     createCanvas(size, size);
     background(255)
+    a = new walker()
+    b = new walker()
+    c = new walker()
 }
 
 function draw() {
-    fill(r, g, b, 255 / 2)
-    noStroke()
-    rect(x, y, psize, psize)
-    if (x <= 0) {
-        x = size / 2;
-    } else if (x >= size) {
-        x = size / 2;
-    }
-    if (y <= 0) {
-        y = size / 2;
-    } else if (y >= size) {
-        y = size / 2;
-    }
-    r = r + random([-rng, rng]) * random(1);
-    g = g + random([-rng, rng]) * random(1);
-    b = b + random([-rng, rng]) * random(1);
-    x = x + random([-1, 0, 1]) * psize
-    y = y + random([-1, 0, 1]) * psize
-
-
-
+    a.update()
+    b.update()
+    c.update()
 }
 
 function mousePressed() {
     window.location.reload()
 
+}
+
+class walker {
+    constructor() {
+        this.x = size / 2;
+        this.y = size / 2;
+        this.r = 50;
+        this.g = 200;
+        this.b = 100;
+        this.rng = 10
+    }
+    update() {
+        fill(this.r, this.g, this.b, 255 / 2)
+        noStroke()
+        rect(this.x, this.y, psize, psize)
+        if (this.x <= 0) {
+            this.x = size / 2;
+        } else if (this.x >= size) {
+            this.x = size / 2;
+        }
+        if (this.y <= 0) {
+            this.y = size / 2;
+        } else if (this.y >= size) {
+            this.y = size / 2;
+        }
+        this.r = this.r + random([-this.rng, this.rng]) * random(1);
+        this.g = this.g + random([-this.rng, this.rng]) * random(1);
+        this.b = this.b + random([-this.rng, this.rng]) * random(1);
+        this.x = this.x + random([-1, 0, 1]) * psize
+        this.y = this.y + random([-1, 0, 1]) * psize
+
+
+    }
 }
